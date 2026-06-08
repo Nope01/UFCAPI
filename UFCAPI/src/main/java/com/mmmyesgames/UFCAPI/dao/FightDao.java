57c9273e6a22039cc690197f831ee79df679640a
Nope01 <notadam18@gmail.com>
2026-06-08 11:11:39 +1000

    Updated database url to reflect server address

--- UFCAPI/src/main/java/com/mmmyesgames/UFCAPI/dao/FightDao.java
+++ UFCAPI/src/main/java/com/mmmyesgames/UFCAPI/dao/FightDao.java
@@ -1,7 +1,7 @@
 package com.mmmyesgames.UFCAPI.dao;
 
 public class FightDao {
-    public static final String jdbcUrl = "jdbc:postgresql://localhost:5432/UFC";
+    public static final String jdbcUrl = "jdbc:postgresql://192.168.0.18:5432/ufc";
     public static final String username = "postgres";
     public static final String password = "nope";
 
