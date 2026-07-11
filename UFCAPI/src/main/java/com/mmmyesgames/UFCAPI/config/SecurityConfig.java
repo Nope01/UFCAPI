6bb30b10908b076abc4d4738d284cf3825242a8d
Nope01 <notadam18@gmail.com>
2026-07-11 17:42:01 +1000

    fixed origins to all

--- UFCAPI/src/main/java/com/mmmyesgames/UFCAPI/config/SecurityConfig.java
+++ UFCAPI/src/main/java/com/mmmyesgames/UFCAPI/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of("http://localhost:63343"));
+                    config.setAllowedOrigins(List.of("*"));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
                     config.setAllowCredentials(true);
